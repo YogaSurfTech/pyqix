071cca1729a05fd6e6d209daeb734771ea9bbb39
Lutz Dornbusch <ldornbusch@yogasurftech.de>
2021-10-03 04:13:57 +0200

    loading logo scaling game screen to window blitting it onto surface

--- pyqix.py
+++ pyqix.py
@@ -1,17 +1,50 @@
+import os
+
 import pygame
 from pygame.locals import *
 
+WIDTH = 1024
+HEIGHT = 1024
 WINDOW_WIDTH = 1024
 WINDOW_HEIGHT = 1024
+X_SCALE = (WIDTH / 256.0)
+Y_SCALE = (HEIGHT / 256.0)
 
+screen = None
 window_surface = None
+logo = None
+
+
+def hal_blt(img, coords):
+    screen.blit(img, (coords[0] * X_SCALE, coords[1] * Y_SCALE))
+
+
+def hal_load_image(fullname, color_key=None):
+    try:
+        image = pygame.image.load(fullname)
+    except pygame.error as message:
+        print('Cannot load image:', fullname)
+        raise SystemExit(message)
+    image = image.convert()
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key, RLEACCEL)
+    return image, image.get_rect()
+
+
+def paint_game():
+    hal_blt(logo, (24, 16))
 
 
 def init():
-    global window_surface
+    global window_surface, screen, logo
     pygame.init()
     pygame.mixer.init()
     window_surface = pygame.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
+    screen = pygame.Surface((WIDTH, HEIGHT))
+    logo, _ = hal_load_image(os.path.join('data', 'qix_logo.png'))
+    logo = pygame.transform.scale(logo, (int(56.0 * X_SCALE), int(20 * Y_SCALE)))
 
 
 def gameloop():		# http://www.koonsolo.com/news/dewitters-gameloop/
@@ -19,6 +52,11 @@ def gameloop():		# http://www.koonsolo.com/news/dewitters-gameloop/
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
+        screen.fill(0)
+        paint_game()
+        window_tmp = pygame.transform.scale(screen, (WINDOW_WIDTH, WINDOW_HEIGHT))
+        window_surface.blit(window_tmp, (0, 0))
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
