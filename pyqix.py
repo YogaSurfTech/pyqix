a73100939107ea7c1b23724ab28c5d1100f57601
Lutz Dornbusch <ldornbusch@yogasurftech.de>
2021-10-03 03:29:52 +0200

    initial import - intro message

---
+++ pyqix.py
@@ -0,0 +1,8 @@
+
+if __name__ == '__main__':
+    print("pyQix")
+    print("-----")
+    print("(c) 2021 YogaSurfTech) https://github.com/YogaSurfTech/pyqix")
+    print("A faithful remake of the Taito classic arcade game QIX in Python. ")
+    print("On the occasion of the 40th anniversary of the release at 18th. October 2021")
+
